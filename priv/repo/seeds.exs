be70f85e567a462b4d6ed4bbb583e1c2afedf6db
Daniel Leal <danielleal94@gmail.com>
2022-07-11 17:26:47 -0300

    added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,65 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     BhubClients.Repo.insert!(%BhubClients.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias BhubClients.Data.Client
+alias BhubClients.Repo
+
+client1 = %{
+  company_name: "Julio e Raul Contábil ME",
+  phone: "19987591184",
+  postal_code: "66050105",
+  district: "Jardim Paulistano",
+  state: "SP",
+  city: "Americana",
+  address: "Rua Itália",
+  number: "441",
+  complement: nil,
+  invoicing: 5840.20,
+  bank_data: %{
+    accounts: [
+      %{
+        account: "54815",
+        agency: "0001",
+        bank: "Itaú"
+      }
+    ]
+  }
+}
+
+Repo.insert!(client1)
+
+client2 = %Client{
+  company_name: "Oliver e Fernanda Doces & Salgados ME",
+  phone: "98993100245",
+  postal_code: "65086289",
+  district: "Anjo da Guarda",
+  state: "MA",
+  city: "São Luís",
+  address: "Rua Síria",
+  number: "130",
+  complement: nil,
+  invoicing: 8574.20
+}
+
+Repo.insert!(client2)
+
+client3 = %Client{
+  company_name: "Andreia e Maya Telecomunicações Ltda",
+  phone: "71985142972",
+  postal_code: "41680410",
+  district: "Patamares",
+  state: "BA",
+  city: "Salvador",
+  address: "Rua Pequiá",
+  number: "767",
+  complement: nil,
+  invoicing: 12000.00,
+  bank_data: %{
+    accounts: [
+      %{
+        account: "98751",
+        agency: "0001",
+        bank: "Bradesco"
+      }
+    ]
+  }
+}
+
+Repo.insert!(client3)
