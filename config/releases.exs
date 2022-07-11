59d6781c258b0a8ce161fbb7a2a303d01205acab
Daniel Leal <danielleal94@gmail.com>
2022-07-11 03:10:21 -0300

    set elixir and erlang versions for deploy

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :bhub_clients, BhubClientsWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
